66081177a991d22679ea362d21560aa139ec8309
Nacho5901 <ignaciojmm2001@gmail.com>
2023-11-17 11:10:40 -0300

    MostrarDocumento

--- script.js
+++ script.js
@@ -18,17 +18,36 @@ function calcularIMC() {
 }
 
 function obtenerRutinaEjercicios(imc, sexo) {
+    var rutinaElement = document.getElementById('rutina');
+
     if (imc < 18.5) {
+        mostrarDocumento(C:\Users\Ryzen 5 3600\Documents\TallerITU\bajo_peso.html, rutinaElement);
         return 'Rutina de ejercicios recomendada para bajo peso.';
     } else if (imc >= 18.5 && imc < 24.9) {
+        mostrarDocumento(peso_normal.html, rutinaElement);
         return 'Rutina de ejercicios recomendada para peso normal.';
     } else if (imc >= 25 && imc < 29.9) {
+        mostrarDocumento('sobrepeso.html', rutinaElement);
         return 'Rutina de ejercicios recomendada para sobrepeso.';
     } else {
         if (sexo === 'masculino') {
+            mostrarDocumento('obesidad_hombres.html', rutinaElement);
             return 'Rutina de ejercicios recomendada para obesidad en hombres.';
         } else {
+            mostrarDocumento('obesidad_mujeres.html', rutinaElement);
             return 'Rutina de ejercicios recomendada para obesidad en mujeres.';
         }
     }
+}
+
+function mostrarDocumento(documento, elemento) {
+    // Carga el contenido del documento en el elemento especificado
+    fetch(documento)
+        .then(response => response.text())
+        .then(content => {
+            elemento.innerHTML = content;
+        })
+        .catch(error => {
+            console.error('Error al cargar el documento:', error);
+        });
 }
