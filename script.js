2e7ff9abbef8bde51e7b248460786d20246327d9
Nacho5901 <ignaciojmm2001@gmail.com>
2023-11-21 00:37:12 -0300

    8.1PruebaVerDOC

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function obtenerRutinaEjercicios(imc, sexo) {
 
 function mostrarDocumento(documento) {
     // Carga el contenido del documento desde GitHub Pages
-    fetch(`https://nacho5901.github.io/TallerITU/${documento}`)
+    fetch(`https://Nacho5901.github.io/TallerITU/${documento}`)
         .then(response => response.text())
         .then(content => {
             var rutinaElement = document.getElementById('rutina');
